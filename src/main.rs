29cb0cc15148ee85a773410f82591b11b7f5deed
zhn <zzihan0805@gmail.com>
2025-02-08 01:42:59 -0500

    start integrating parser with rodio

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{self, Write}, fs::{self}};
+use std::{fs::{self}, io::{self, Write}, str::Bytes};
 use rodio::{source, OutputStream, OutputStreamHandle, Sink, Source};
 
 mod parser;
@@ -10,13 +10,14 @@ fn main() {
         .unwrap();
     let mut file_name = String::new();
     
+    // Loop until input is recieved
     loop {
         print!("Enter file path: ");
         io::stdout().flush().unwrap();
 
         match io::stdin().read_line(&mut file_name) {
             Ok(_) => {
-                let file_name = file_name.trim();
+                let file_name = file_name.trim(); // crucial to fixing file-read error due to '\n' char
                 match fs::read_to_string(file_name) {
                     Ok(_) => break,
                     Err(e) => println!("Can not access file (check file director): {}", e)
@@ -25,11 +26,11 @@ fn main() {
             Err(_) => println!("Failed to read input. Try again."),
         }
     }
-    
 
-    let source_code= parser::read_file(&file_name.trim());
     let tokens = parser::tokenize(&source_code);
     println!("{:?}", tokens);
+
+
 
     let melody: Vec<&str> = vec!["D4", "E4", "F4", "G4", "E4", "C4", "D4"];
 
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use std::{fs::{self, File}, io::Read, path::PathBuf};
+use std::{fs::{File}, io::{BufReader, Read}};
 
 // let x = 45
 
@@ -12,7 +12,8 @@ pub enum TokenType {
     Number(f64),
 
     AssignExpr,
-    Identifier,
+    Identifier(String),
+    Comma, // separator
     OpenParen,
     CloseParen,
     OpenBrace,
@@ -23,43 +24,38 @@ pub enum TokenType {
     EOF
 }
 
-struct Lexer {
-    value: String,
-    token: TokenType,
+pub fn read_file(file_path:  &str) -> Result<String, std::io::Error> {
+    let file = File::open(file_path)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut source_code = String::new();
+    buf_reader.read_to_string(&mut source_code)?;
+    Ok(source_code)
 }
 
-pub fn read_file(file_path:  &str) -> String {
-    println!("Reading file");
-
-    let source_code = fs::read_to_string(&file_path)
-        .unwrap_or_else(|e| panic!("Could not read file: {}", e));
-
-    println!("Successfully read file");
-    source_code
-}
-
+// Lexing
 pub fn tokenize(source_code: &str) -> Vec<TokenType> {
     let mut tokens: Vec<TokenType> = Vec::new();
     let mut chars = source_code.chars().peekable();
     let mut line_number = 1;
-
-    println!("{}", &source_code);
 
     while let Some(ch) = chars.next() {
         match ch {
             // White space
             ' ' | '\r' | '\t' => (),
 
-            '\n' => { line_number+=1; }
+            '\n' => { line_number+=1; },
+
+            ',' => { tokens.push(TokenType::Comma) }
 
             // Numbers
             '0'..='9' => {
                 let mut number = String::new();
                 number.push(ch);
 
-                while let Some(ch) = chars.next() {
+                while let Some(&ch) = chars.peek() {
                     if ch.is_digit(10) || ch == '.' {
                         number.push(ch);
+                        chars.next();
                     } else {
                         break;
                     }
@@ -73,7 +69,7 @@ pub fn tokenize(source_code: &str) -> Vec<TokenType> {
                 identifier.push(ch);
 
                 while let Some(ch) = chars.next() {
-                    if ch.is_ascii_alphanumeric() {
+                    if ch.is_ascii_alphanumeric() || ch == '#' {
                         identifier.push(ch);
                     } else {
                         break;
@@ -87,13 +83,14 @@ pub fn tokenize(source_code: &str) -> Vec<TokenType> {
                     "MusicSheet" => TokenType::MusicSheet,
                     other => {
                         let chars: Vec<char> = other.chars().collect();
+                        println!("Chars: {:?}", chars);
                         match chars.as_slice() {
-                            [note, octave] if ('A'..='G').contains(note) 
-                                && ('0'..='8').contains(octave) => TokenType::Note(other.to_string()),
                             [note, acc, octave] if ('A'..='G').contains(note)
                                 && ['b', '#'].contains(acc)
                                 && ('0'..='8').contains(octave) => TokenType::Note(other.to_string()),
-                            _ => TokenType::Identifier
+                            [note, octave] if ('A'..='G').contains(note)
+                                && ('0'..='8').contains(octave) => TokenType::Note(other.to_string()),
+                            _ => TokenType::Identifier(other.to_string())
                         }
                     }
                 };
@@ -125,4 +122,145 @@ pub fn tokenize(source_code: &str) -> Vec<TokenType> {
 
     tokens.push(TokenType::EOF);
     tokens
+}
+
+#[derive(Debug)]
+struct Note {
+    note: String,
+    duration: f64,
+}
+
+#[derive(Debug)]
+enum AstNode {
+    MusicSheet {
+        notes: Vec<Note>
+    },
+    Bpm(f64),
+    PlayKeyword
+}
+
+struct Parser {
+    tokens: Vec<TokenType>,
+    current: usize,
+}
+
+// Parsing
+impl Parser {
+    fn new(tokens: Vec<TokenType>) -> Self {
+        Parser {
+            tokens,
+            current: 0
+        }
+    }
+
+    fn advance(&mut self) -> Option<&TokenType> {
+        let token = self.tokens.get(self.current);
+        self.current += 1;
+        token
+    }
+
+    fn peek(&self) -> Option<&TokenType> {
+        self.tokens.get(self.current)
+    }
+
+    fn parse_program(&mut self) -> Result<Vec<AstNode>, String> {
+        let mut nodes = Vec::new();
+
+        while let Some(token) = self.peek() {
+            match token {
+                TokenType::Bpm(_) => {
+                    nodes.push(self.parse_bpm()?);
+                },
+                TokenType::MusicSheet => {
+                    nodes.push(self.parse_music_sheet()?);
+                },
+                TokenType:: Play => {
+                    self.advance();
+                    nodes.push(AstNode::PlayKeyword);
+                },
+                TokenType::EOF => break,
+                _ => return Err(format!("Unexpected token {:?}: ", token))
+            }
+        }
+        Ok(nodes)
+    }
+
+    fn parse_bpm(&mut self) -> Result<AstNode, String>{
+        match self.advance() {
+            Some(TokenType::Bpm(value)) => Ok(AstNode::Bpm(*value)),
+            _ => Err(String::from("Expected BPM Value"))
+        }
+    }
+
+    fn parse_music_sheet(&mut self) -> Result<AstNode, String> {
+        match self.advance() {
+            Some(TokenType::MusicSheet) => (),
+            _ => return Err(String::from("Expected Music Sheet"))
+        }
+
+        match self.advance() {
+            Some(TokenType::AssignExpr) => (),
+            _ => return Err(String::from("Expected ="))
+        }
+
+        match self.advance() {
+            Some(TokenType::OpenBrace) => (),
+            _ => return Err(String::from("Expected {"))
+        }
+
+        let mut notes = Vec::new();
+
+        while let Some(token) = self.peek() {
+            match token {
+                TokenType::CloseBrace => {
+                    self.advance();
+                    break;
+                },
+                TokenType::Note(_) => {
+                    notes.push(self.parse_note()?);
+                    
+                    if let Some(next_token) = self.peek() {
+                        match next_token {
+                            TokenType::CloseBrace => continue,
+                            TokenType::Comma => {
+                                self.advance();
+                            },
+                            _ => return Err(String::from("Expected comma between notes"))
+                        }
+                    }
+                },
+                _ => return Err(format!("Unexpected token in music sheet: {:?}", token))
+            }
+        }
+
+        Ok(AstNode::MusicSheet { notes })
+    }
+
+    fn parse_note(&mut self) -> Result<Note, String> {
+        let note_token = self.advance().ok_or("Expected note")?;
+
+        let note = match note_token {
+            TokenType::Note(value) => value.clone(),
+            _ => return Err(String::from("Expected note value"))
+        };
+
+        match self.advance() {
+            Some(TokenType::OpenParen) => (),
+            _ => return Err(String::from("Expected ("))
+        }
+
+        let duration = if let Some(&TokenType::Number(value)) = self.tokens.get(self.current) {
+            self.advance(); 
+            value
+        } else {
+            1.0
+        };
+
+        match self.advance() {
+            Some(TokenType::CloseParen) => (),
+            _ => return Err(String::from("Expected )"))
+        }   
+
+        Ok(Note { note, duration } )
+    }
 }
